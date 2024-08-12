978a7c076ddf5a8b27c13b3af994dbe14a0633c4
Petrus Pierre <petrus.tsx@gmail.com>
2024-08-12 18:58:33 -0300

    fix build

--- src/server.ts
+++ src/server.ts
@@ -13,46 +13,50 @@ import type { ParityController } from "./infra/http/parity-controller.js";
 
 scrapingJob.start();
 
-const sourceController = appContainer.get<SourceController>(CONTROLLERS.SOURCE);
-const partnerController = appContainer.get<PartnerController>(
-	CONTROLLERS.PARTNER,
-);
-const parityController = appContainer.get<ParityController>(CONTROLLERS.PARITY);
+const setupApi = async () => {
+	const sourceController = appContainer.get<SourceController>(CONTROLLERS.SOURCE);
+	const partnerController = appContainer.get<PartnerController>(
+		CONTROLLERS.PARTNER,
+	);
+	const parityController = appContainer.get<ParityController>(CONTROLLERS.PARITY);
+	
+	const server = Fastify({
+		logger: true,
+	});
+	
+	await server.register(import('@fastify/rate-limit'), {
+		max: 150,
+		timeWindow: '1 day',
+	})
+	
+	server.get("/v1/source", sourceController.search);
+	server.get("/v1/source/:sourceId", sourceController.findById);
+	
+	server.get("/v1/partner", partnerController.search);
+	server.get("/v1/partner/:partnerId", partnerController.findById);
+	
+	server.get("/v1/parity/source/:sourceId", parityController.getParitiesBySourceId);
+	
+	server.get(
+		"/v1/parity/source/:sourceId/partner/:partnerId/history",
+		parityController.getParityHistoryBySourceId,
+	);
+	
+	server.post("/v1/refresh", async (request, reply) => {
+		startScraping();
+		return reply.send({ message: "Scraping job started" });
+	});
+	
+	const startServer = async () => {
+		try {
+			await server.listen({ port: env.PORT });
+		} catch (err) {
+			server.log.error(err);
+			process.exit(1);
+		}
+	};
+	
+	startServer();
+}
 
-const server = Fastify({
-	logger: true,
-});
-
-await server.register(import('@fastify/rate-limit'), {
-  max: 150,
-	timeWindow: '1 day',
-})
-
-server.get("/v1/source", sourceController.search);
-server.get("/v1/source/:sourceId", sourceController.findById);
-
-server.get("/v1/partner", partnerController.search);
-server.get("/v1/partner/:partnerId", partnerController.findById);
-
-server.get("/v1/parity/source/:sourceId", parityController.getParitiesBySourceId);
-
-server.get(
-	"/v1/parity/source/:sourceId/partner/:partnerId/history",
-	parityController.getParityHistoryBySourceId,
-);
-
-server.post("/v1/refresh", async (request, reply) => {
-	startScraping();
-	return reply.send({ message: "Scraping job started" });
-});
-
-const startServer = async () => {
-	try {
-		await server.listen({ port: env.PORT });
-	} catch (err) {
-		server.log.error(err);
-		process.exit(1);
-	}
-};
-
-startServer();
+setupApi();
