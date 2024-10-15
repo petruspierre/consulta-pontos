66494b3e02d9773e37d15a17d144ac9ac88c7c45
Petrus Pierre <petrus.tsx@gmail.com>
2024-10-15 09:40:04 -0300

    update rate limiter

--- src/server.ts
+++ src/server.ts
@@ -14,40 +14,47 @@ import type { ParityController } from "./infra/http/parity-controller.js";
 scrapingJob.start();
 
 const setupApi = async () => {
-	const sourceController = appContainer.get<SourceController>(CONTROLLERS.SOURCE);
+	const sourceController = appContainer.get<SourceController>(
+		CONTROLLERS.SOURCE,
+	);
 	const partnerController = appContainer.get<PartnerController>(
 		CONTROLLERS.PARTNER,
 	);
-	const parityController = appContainer.get<ParityController>(CONTROLLERS.PARITY);
-	
+	const parityController = appContainer.get<ParityController>(
+		CONTROLLERS.PARITY,
+	);
+
 	const server = Fastify({
 		logger: true,
 	});
-	
-	await server.register(import('@fastify/rate-limit'), {
-		max: 150,
-		timeWindow: '1 day',
-	})
-	
+
+	await server.register(import("@fastify/rate-limit"), {
+		max: 100,
+		timeWindow: "1 minute",
+	});
+
 	server.get("/v1/source", sourceController.search);
 	server.get("/v1/source/:sourceId", sourceController.findById);
-	
+
 	server.get("/v1/partner", partnerController.search);
 	server.get("/v1/partner/:partnerId", partnerController.findById);
-	
-	server.get("/v1/parity/source/:sourceId", parityController.getParitiesBySourceId);
-	
+
+	server.get(
+		"/v1/parity/source/:sourceId",
+		parityController.getParitiesBySourceId,
+	);
+
 	server.get(
 		"/v1/parity/source/:sourceId/partner/:partnerId/history",
 		parityController.getParityHistoryBySourceId,
 	);
-	
+
 	server.post("/v1/refresh", async (request, reply) => {
 		startScraping();
 		return reply.send({ message: "Scraping job started" });
 	});
-	
+
 	await server.listen({ port: env.PORT });
-}
+};
 
 setupApi();
